6b836ebcefcb6d2401e0cbe284704f34a8578218
Yanet Villa Rojo <yanetrojo01@gmail.com>
2022-06-24 00:39:39 -0500

    SignUp desborde arreglado

--- users_management_flutter_app/lib/Pages/SignUpPage.dart
+++ users_management_flutter_app/lib/Pages/SignUpPage.dart
@@ -41,98 +41,106 @@ class SignUpPage extends StatelessWidget {
           foregroundColor: Global.colorBase,
         ),
         backgroundColor: Global.colorBack,
-        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          const Text(
-            "Sign Up",
-            style: TextStyle(
-                height: 1,
-                fontSize: 23,
-                color: Global.colorBase,
-                fontWeight: FontWeight.bold),
-          ),
-          const Padding(
-              padding: EdgeInsets.only(top: 30.0, bottom: 20.0),
-              child: Image(
-                image: NetworkImage(
-                    'https://cdn.discordapp.com/attachments/956669281813299230/989685249262907403/add-contact.png'),
-                height: 120.0,
-              )),
-          TextFieldWidget(
-              isPrefixIcon: false,
-              isSuffixIcon: false,
-              isMyControllerActivated: true,
-              controller: _controllerTxtName,
-              onChanged: (value){
-                name = value;
-              },
-              hintText: "Firstname"),
-          TextFieldWidget(
-              isPrefixIcon: false,
-              isSuffixIcon: false,
-              isMyControllerActivated: true,
-              controller: _controllerTxtLastName,
-              onChanged: (value){
-                lastName = value;
-              },
-              hintText: "Lastname"),
-          TextFieldWidget(
-              isPrefixIcon: false,
-              isSuffixIcon: false,
-              isMyControllerActivated: true,
-              controller: _controllerTxtEmail,
-              onChanged: (value){
-                email = value;
-              },
-              hintText: "E-mail"),
-          TextFieldWidget(
-              isPrefixIcon: false,
-              isSuffixIcon: false,
-              isMyControllerActivated: true,
-              controller: _controllerTxtPassword,
-              onChanged: (value){
-                password = value;
-              },
-              hintText: "Password"),
-          ButtonWidget(
-              colorButton: Global.colorBase,
-              color: Global.colorBack,
-              title: "Sign Up",
-              hasColor: false,
-              onPressed: () {
-                var missingFields = '';
-                var isNotEmpty = true;
-                if(_controllerTxtName.text.isEmpty){
-                  isNotEmpty = false;
-                  missingFields += 'name ';
-                }
-                if(_controllerTxtLastName.text.isEmpty){
-                  isNotEmpty = false;
-                  missingFields += 'nast name ';
-                }
-                if(_controllerTxtEmail.text.isEmpty){
-                  isNotEmpty = false;
-                  missingFields += 'email ';
-                }
-                if(_controllerTxtPassword.text.isEmpty){
-                  isNotEmpty = false;
-                  missingFields += 'password ';
-                }
-                if(isNotEmpty){
-                   users
-                    .add({'name': name, 'lastname': lastName, 'email': email, 'password': password, 'admin': admin})
-                    .then((value) => print('User Added'))
-                    .catchError(
-                      (error) => print('Failed to add user: $error'));
-                  if(admin){
-                    Navigator.of(context).push(MaterialPageRoute(
-                    builder: (BuildContext context) => UsersPage()));
-                  }else{
-                    Navigator.of(context).pop();
+        body: ListView(children: [
+          Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            const Text(
+              "Sign Up",
+              style: TextStyle(
+                  height: 1,
+                  fontSize: 23,
+                  color: Global.colorBase,
+                  fontWeight: FontWeight.bold),
+            ),
+            const Padding(
+                padding: EdgeInsets.only(top: 30.0, bottom: 20.0),
+                child: Image(
+                  image: NetworkImage(
+                      'https://cdn.discordapp.com/attachments/956669281813299230/989685249262907403/add-contact.png'),
+                  height: 120.0,
+                )),
+            TextFieldWidget(
+                isPrefixIcon: false,
+                isSuffixIcon: false,
+                isMyControllerActivated: true,
+                controller: _controllerTxtName,
+                onChanged: (value) {
+                  name = value;
+                },
+                hintText: "Firstname"),
+            TextFieldWidget(
+                isPrefixIcon: false,
+                isSuffixIcon: false,
+                isMyControllerActivated: true,
+                controller: _controllerTxtLastName,
+                onChanged: (value) {
+                  lastName = value;
+                },
+                hintText: "Lastname"),
+            TextFieldWidget(
+                isPrefixIcon: false,
+                isSuffixIcon: false,
+                isMyControllerActivated: true,
+                controller: _controllerTxtEmail,
+                onChanged: (value) {
+                  email = value;
+                },
+                hintText: "E-mail"),
+            TextFieldWidget(
+                isPrefixIcon: false,
+                isSuffixIcon: false,
+                isMyControllerActivated: true,
+                controller: _controllerTxtPassword,
+                onChanged: (value) {
+                  password = value;
+                },
+                hintText: "Password"),
+            ButtonWidget(
+                colorButton: Global.colorBase,
+                color: Global.colorBack,
+                title: "Sign Up",
+                hasColor: false,
+                onPressed: () {
+                  var missingFields = '';
+                  var isNotEmpty = true;
+                  if (_controllerTxtName.text.isEmpty) {
+                    isNotEmpty = false;
+                    missingFields += 'name ';
                   }
-                }else{
-                  print("missing fields: " + missingFields);
-                }
-              })
+                  if (_controllerTxtLastName.text.isEmpty) {
+                    isNotEmpty = false;
+                    missingFields += 'nast name ';
+                  }
+                  if (_controllerTxtEmail.text.isEmpty) {
+                    isNotEmpty = false;
+                    missingFields += 'email ';
+                  }
+                  if (_controllerTxtPassword.text.isEmpty) {
+                    isNotEmpty = false;
+                    missingFields += 'password ';
+                  }
+                  if (isNotEmpty) {
+                    users
+                        .add({
+                          'name': name,
+                          'lastname': lastName,
+                          'email': email,
+                          'password': password,
+                          'admin': admin
+                        })
+                        .then((value) => print('User Added'))
+                        .catchError(
+                            (error) => print('Failed to add user: $error'));
+                    if (admin) {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (BuildContext context) => UsersPage()));
+                    } else {
+                      Navigator.of(context).pop();
+                    }
+                  } else {
+                    print("missing fields: " + missingFields);
+                  }
+                })
+          ]),
         ]));
   }
 }
